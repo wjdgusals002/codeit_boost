e338f804224ea02cc1a649b6595b58a4b6109551
jung <nightmr2765@gmail.com>
2024-09-05 22:06:16 +0900

    update cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import cors from 'cors';
 const app= express();
 
 app.use(cors({
-    origin: 'https://localhost:3000'
+    origin: '*',
     }));
 
 app.use(express.json());
@@ -32,8 +32,8 @@ app.use('/api',commentRoutes);
 app.use('/api',badgeRoutes);
 app.use('/api/image',imageRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
